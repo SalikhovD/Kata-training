45d640ed98cf030dfa8e71dc349d9839013222d1
SalikhovD <salikhvdima@gmail.com>
2022-06-12 22:13:49 +0300

    first upload

---
+++ Kata test calculator/src/com/company/CalcExeption.java
@@ -0,0 +1,9 @@
+package com.company;
+
+
+public class CalcExeption extends RuntimeException {
+    public CalcExeption(String description) {
+        super(description);
+
+    }
+}
---
+++ Kata test calculator/src/com/company/Digit.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public enum Digit {
+    I(1), II(2), III(3), IV(4), V(5), VI(6), VII(7), VIII(8), IX(9), X(10),
+    XX(20), XXX(30), XL(40), L(50), LX(60), LXX(70), LXXX(80), XC(90), C(100);
+
+    private int arabic;
+    private String roman;
+
+    Digit(int arabic) {
+        this.arabic = arabic;
+    }
+
+    public int getArabic() {
+        return arabic;
+    }
+
+    //Возвращает римскую запись переданного числа
+    public static String toRoman(int arabic) {
+        Digit[] digits = Digit.values();
+        for (Digit digit : digits) {
+            if (digit.getArabic() == arabic) {
+                return digit.name();
+            }
+        }
+        return "";
+    }
+}
